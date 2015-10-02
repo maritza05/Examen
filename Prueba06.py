db8ed80dc6383bcd1cb9219b6d5b52346fed34a4
super usuario vagrant <vagrant@vagrant.vagrant-debian-amd64>
2015-10-02 10:57:58 -0500

    Cambios para ajustar al estándar pep mediante autopep8

--- Prueba06.py
+++ Prueba06.py
@@ -1,9 +1,10 @@
 class Prueba06():
+
     def get_iguales(self, number_list, k):
-        return [x for x in number_list if x==k]
+        return [x for x in number_list if x == k]
 
     def get_multiplos(self, number_list, k):
-        return [x for x in number_list if x%k==0]
+        return [x for x in number_list if x % k == 0]
 
     def get_mayores(self, number_list, k):
         return [x for x in number_list if x > k]
